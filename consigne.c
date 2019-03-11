277951c7cfbb110aa081f5b95efa6798d10e679b
Antoine Lucas <antlucas@hotmail.fr>
2019-03-11 16:32:16 +0100

    consigne 2.0 avec sécurité verrou fichier

--- consigne.c
+++ consigne.c
@@ -2,15 +2,19 @@
 
 // Fonction qui permet de récupérer la valeur de la commande de chauffage du fichier consigne.txt
 float consigne(float csgn){
-   FILE * fichier = NULL;
+   FILE * fichier, *verrouFile = NULL;
+   verrouFile = fopen(".verrouConsigne", "r");
 
-   fichier = fopen("consigne.txt", "r"); // Permet d'ouvrir le fichier consigne.txt
+   if (verrouFile == NULL){
+      fichier = fopen("consigne.txt", "r"); // Permet d'ouvrir le fichier consigne.txt
 
-   if (fichier != NULL){
-      fscanf(fichier, "%f", &csgn); // Lit la première valeur du fichier
-      fclose(fichier);
-   }else{
-      printf("Impossible d'ouvrir le fichier consigne.txt");
+      if (fichier != NULL){
+         fscanf(fichier, "%f", &csgn); // Lit la première valeur du fichier
+         fclose(fichier);
+      }
+      else{
+         printf("Impossible d'ouvrir le fichier consigne.txt");
+      }
    }
    return csgn;
 }

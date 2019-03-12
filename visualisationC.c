f090f463f287d593511153a53d9803eae41fa704
Antoine Lucas <antlucas@hotmail.fr>
2019-03-12 11:49:53 +0100

    commit validation visualisationT et visualisationC

--- visualisationC.c
+++ visualisationC.c
@@ -5,28 +5,30 @@
 
 void visualisationC(float puissance_f){
    FILE *fichier, *verrouFile = NULL;
-   //float tempExt = 0;
-   //float tempInt = 0;
+   float tempExt, tempInt;
+   char *chaine = malloc(8 * sizeof(char));
 
    if (access(".verrouData", F_OK) == -1){      // On vérifie que le fichier data.txt est pas déjà en lecture
       verrouFile = fopen(".verrouData", "w");   // Crée le fichier .verrouData afin de signifier que le fichier data.txt est en utilisation
 
-      fichier = fopen("data.txt", "a"); // Permet d'ouvrir le fichier data.txt
-
+      fichier = fopen("data.txt", "r"); // Permet d'ouvrir le fichier data.txt
       if (fichier != NULL){
-         //fscanf(fichier, "%f\n%f\n", &tempExt, &tempInt);
-         //printf("%f\n%f\n", tempExt, tempInt);
+         fscanf(fichier, "%f%f%s", &tempExt, &tempInt, chaine);
+         fclose(fichier);
+      }
+
+      fichier = fopen("data.txt", "w");
+      if (fichier != NULL){
          if(puissance_f == 0){
-            fprintf(fichier, "false");
+            fprintf(fichier, "%.2f\n%.2f\nfalse", tempExt, tempInt);
          }else if(puissance_f > 0){
-            fprintf(fichier, "true");
+            fprintf(fichier, "%.2f\n%.2f\ntrue", tempExt, tempInt);
          }
          fclose(fichier);
          fclose(verrouFile);
          remove(".verrouData");
-      }else{
-         printf("Impossible d'ouvrir le fichier data.txt");
       }
+
    }else{
       printf("\n\n Le fichier data est déjà ouvert\n\n");
    }
--- visualisationT.c
+++ visualisationT.c
@@ -1,43 +1,29 @@
 #include "visualisationT.h"
 #include <unistd.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 
 void visualisationT(temp_t myTemp){
-    FILE* fichier;
-    FILE *verrouFile;
-    float tempExt;
-    float tempInt;
+    FILE *fichier, *verrouFile = NULL;
+    float tempExt, tempInt;
     char* chaine = malloc(8*sizeof(char));
-    //printf("%d",access(".verrouData", F_OK));
 
-        if (access(".verrouData", F_OK) != -1){ // On vérifie que le fichier data.txt est pas déjà en lecture
-        }else{
-            verrouFile = fopen(".verrouData", "w");
-       
-            fichier = fopen("data.txt", "r"); // Permet d'ouvrir le fichier data.txt
-            printf("Erreur8");
-            //if (fichier != NULL){
-                fscanf(fichier, "%f", &tempExt);
-                printf("Erreur7");
-                fscanf(fichier, "%f", &tempInt);
-                printf("Erreur9");
-                fscanf(fichier, "%s", chaine);
-                printf("Erreur1");
-                //fclose(fichier);
-            //}
-            //fichier = fopen("data.txt", "w+");
-            //if (fichier != NULL){
-                printf("Erreur2");
-                fprintf(fichier, "%.2f\n", myTemp.exterieure);
-                fprintf(fichier, "%.2f\n", myTemp.interieure);
-                fprintf(fichier, "%s", chaine);
-                printf("Erreur3");
-                fclose(fichier);
-                fclose(verrouFile);
-                remove(".verrouData");
-            /*}else{
-                printf("Erreur");
-            }*/
+    if (access(".verrouData", F_OK) == -1){ // On vérifie que le fichier data.txt est pas déjà en lecture
+        verrouFile = fopen(".verrouData", "w");
+
+        fichier = fopen("data.txt", "r"); // Permet d'ouvrir le fichier data.txt
+        if (fichier != NULL){
+            fscanf(fichier, "%f%f%s", &tempExt, &tempInt, chaine);
+            fclose(fichier);
         }
+
+        fichier = fopen("data.txt", "w");
+        if (fichier != NULL){
+            fprintf(fichier, "%.2f\n%.2f\n%s", myTemp.exterieure, myTemp.interieure, chaine);
+            fclose(fichier);
+            fclose(verrouFile);
+            remove(".verrouData");
+        }
+    }
 }
 

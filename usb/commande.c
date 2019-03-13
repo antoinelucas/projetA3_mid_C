71135435eb0f51fce7f92c685a5256be74397146
lologit1998 <37800907+lologit1998@users.noreply.github.com>
2019-03-13 16:02:26 +0100

    usb command

--- usb/commande.c
+++ usb/commande.c
@@ -6,26 +6,15 @@ void commande(FT_HANDLE ftHandle, float cmd){
     // on utilise FT_WRITE
     FT_STATUS ftStatus;
     DWORD BytesWritten = 1;
-    char TxBuffer[256];// Contains data to write to device
-    float puissance = (cmd/127)*100;
-    ftStatus = FT_Open(0, &ftHandle);
-    if(ftStatus != FT_OK) {
-        // FT_Open failed
-    }
+    char TxBuffer[1];// Contains data to write to device
+    printf("CMD : %f\n",cmd);
+    TxBuffer[0] = cmd*1.27;
+    printf("TxBuffer : %u\n",TxBuffer[0]);
+
     ftStatus = FT_Write(ftHandle, TxBuffer, sizeof(TxBuffer), &BytesWritten);
     if (ftStatus == FT_OK) {
-        if((int)puissance==0){
-            TxBuffer[0]= (int)cmd & 0x7F;
-            TxBuffer[0] &= ~(1<<7);
-        }else{
-            TxBuffer[0]= (int)cmd & 0x7F;
-            TxBuffer[0] |= (1<<7);
-        }
         // FT_Write OK
     }else{
         // FT_Write Failed
     }
-    printf("Value of TxBUFFER[0]:%d\n",TxBuffer[0]);
-    FT_Close(ftHandle);
 }
-
--- usb/main.c
+++ usb/main.c
@@ -1,19 +1,22 @@
 #include "..\define.h"
 #include "releve.h"
+#include "commande.h"
 
 int main(){
     FT_HANDLE descr;
     temp_t *temperature;
     temperature = malloc(1 * sizeof(temp_t));
     descr = initUSB();
-    releve(temperature, descr);
-    commande(descr,127);
-
+    
+    do{
+        releve(temperature, descr);
+        commande(descr,0);
     //temperature.exterieure, temperature.interieure =  releve();
     //temperature.exterieure = tempExtCelsius;
     //temperature.interieure = tempIntCelsius;
-    printf("%.2f Celsius\n", temperature->exterieure);
-    printf("%.2f Celsius\n", temperature->interieure);
+        printf("%.2f Celsius Ext\n", temperature->exterieure);
+        printf("%.2f Celsius Int\n\n\n", temperature->interieure);
+    }while(1);
     free(temperature);
     finUSB(descr);
     return 0;

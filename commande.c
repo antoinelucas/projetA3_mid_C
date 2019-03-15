ce37015defc4f433546b878c179fb3fad26ff2dc
Antoine Lucas <antlucas@hotmail.fr>
2019-03-15 10:37:27 +0100

    commit update comments

--- commande.c
+++ commande.c
@@ -8,7 +8,7 @@ void commande(FT_HANDLE ftHandle, float cmd){
     DWORD BytesWritten = 1;
     char TxBuffer[1];           // Contient les valeurs à écrire sur la carte
     printf("CMD : %f\n",cmd);   // Affiche la valeur de la puissance 
-    TxBuffer[0] = (cmd / 127) * 100;     // Permet d'avoir la puissance en pourcentage
+    TxBuffer[0] = (cmd / 127) * 100;     // Permet d'avoir la valeur de la puissance
     printf("TxBuffer : %u\n",TxBuffer[0]);  // Affiche la valeur que l'on envoie à la carte
 
     ftStatus = FT_Write(ftHandle, TxBuffer, sizeof(TxBuffer), &BytesWritten);

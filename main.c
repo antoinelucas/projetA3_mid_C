b1d34413ed01cd491b21bea0d02f00b32e76edf3
Antoine Lucas <antlucas@hotmail.fr>
2019-03-15 11:57:26 +0100

    update main.c

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 
 int main(){
     //Déclaration
-    FT_HANDLE *descr;
+    FT_HANDLE descr;
     temp_t temperature;
     float csgn, cmd = 0.0;
     int regul = 0;
@@ -26,20 +26,20 @@ int main(){
     descr = initUSB(); // Initialise la connexion USB
 
     do{
-        temperature = releve(temperature, &descr);   // Récupère les valeurs de températures de la carte via USB
+        temperature = releve(temperature, descr);   // Récupère les valeurs de températures de la carte via USB
         pid->newV = temperature.interieure;
         pid->newI = (csgn - temperature.interieure);
         visualisationT(temperature);    // Permet de mettre les températures dans le fichier data.txt
         csgn = consigne(csgn);          // Permet de récupérer la valeur de consigne du fichier consigne.txt
-        cmd = regulation(regul, csgn, temperature.interieure, pid, i,mode_PID);  // Retourne la valeur de la puissance
+        cmd = regulation(regul, csgn, temperature.interieure, pid, i, mode_PID);  // Retourne la valeur de la puissance
         visualisationC(cmd);    // Ecrit dans data.txt si le chauffage est en route ou non
-        commande(&descr,cmd);    // Envoie la valeur de la puissance (en %) à la carte via USB
+        commande(descr,cmd);    // Envoie la valeur de la puissance (en %) à la carte via USB
 
         printf("%.2f Celsius Ext\n", temperature.exterieure);
         printf("%.2f Celsius Int\n\n\n", temperature.interieure);
-    }while( csgn > 5 && csgn < 40);
+    }while( csgn > 5 && csgn <= 40);
 
     free(pid);      // On libère l'espace réservé pour la structure SPID
-    finUSB(&descr);  // On ferme la liaison USB
+    finUSB(descr);  // On ferme la liaison USB
     return 0;
 }
